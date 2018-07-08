925f24ea872fcc88ae801d172031db34d3c83ead
andyyang <r06922094@ntu.edu.tw>
2018-07-08 11:52:32 +0800

    Add interface and connect model op to task

---
+++ example/interface.py
@@ -0,0 +1,43 @@
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+def G_f(optimizer, nn_input, nn_graph, batch_size, device_id):
+    optimizer.zero_grad()
+    #gen_imgs = generator(z)
+    gen_imgs = nn_graph(nn_input)
+    return gen_imgs
+
+def G_b(optimizer, nn_input_1, nn_input_2, nn_graph, batch_size, device_id):
+    #gen.forward(z).backward(g_grad_recv)
+    nn_graph.forward(nn_input_1).backward(nn_input_2)
+    optimizer.step()
+
+def D_upG_fb(nn_input, nn_graph, batch_size, device_id):
+    adversarial_loss = torch.nn.BCELoss()
+    valid = Variable(Tensor(batch_size, 1).fill_(1.0), requires_grad=False).cuda(device_id)
+    #g_loss = adversarial_loss(discriminator(d_input.cuda(1)), valid).cuda(1)
+    g_loss = adversarial_loss(nn_graph(nn_input), valid).cuda(device_id)
+    g_loss.backward()
+
+    return nn_input.grad.data
+
+def D_upD_fb(optimizer, gen_input, real_input, nn_graph, batch_size, device_id):
+    adversarial_loss = torch.nn.BCELoss()
+    valid = Variable(Tensor(batch_size, 1).fill_(1.0), requires_grad=False).cuda(device_id)
+    fake  = Variable(Tensor(batch_size, 1).fill_(0.0), requires_grad=False).cuda(device_id)
+
+    real_loss = adversarial_loss(nn_graph(real_input), valid).cuda(device_id)
+    fake_loss = adversarial_loss(nn_graph(gen_input ), fake ).cuda(device_id)
+    d_loss = (real_loss + fake_loss) / 2
+
+    d_loss.backward()
+    optimizer.step()
+
--- task.py
+++ task.py
@@ -1,14 +1,56 @@
+import example.interface
+
+
 class task_t:
     # Description of tasks
     # f : feedforward @ Generator
     # b : backforward @ Generator
     # fbnu : feedforward + backforward (no-update) @ Discriminator
     # fbu : feedforward + backforward @ Discriminator
-    def __init__(self, generator_index, task_type, nn_network, nn_input):
+    def __init__(self, generator_index, task_type, nn_network, nn_input_1, nn_input_2, device_id):
+        self.device_id = device_id
         self.generator_index = generator_index
         self.task_type = task_type
         self.nn_network = nn_network
-        self.nn_input = nn_input
+        self.nn_input_1 = nn_input_1
+        self.nn_input_2 = nn_input_2
+
+        self.batch_size = 32
+        self.optimizer = torch.optim.Adam(self.nn_network.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+    def learn(self):
+
+        # G task
+        if self.task_type == 'f':
+            self.nn_input_1 = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))), requires_grad=True).cuda(self.device_id)
+
+            gen_imgs = G_f(self.optimizer, z, self.nn_network, self.batch_size, self.device_id)
+            d_input = Variable(gen_imgs, requires_grad = True)
+
+            # input_noise, input_fake_img
+            return self.nn_input_1, d_input
+
+        elif self.task_type == 'b':
+            self.nn_input_1 = self.nn_input_1.cuda(self.device_id)
+            self.nn_input_2 = self.nn_input_2.cuda(self.device_id)
+
+            G_b(self.optimizer, self.nn_input_1, self.nn_input_2, self.nn_network, self.batch_size, self.device_id)
+
+        # D task
+        elif self.task_type == 'fbnu':
+            self.nn_input_1 = self.nn_input_1.cuda(self.device_id)
+            
+            # self.nn_graph = discriminator
+            gen_grad = D_upG_fb(self.nn_input_1, self.nn_network, self.batch_size, self.device_id)
+            g_grad_send = Variable(gen_grad, requires_grad = True)
+            # need to return z
+            return g_grad_send, self.nn_input_2
+
+        elif self.task_type == 'fbu':
+            self.nn_input_1 = self.nn_input_1.cuda(self.device_id)
+            self.nn_input_2 = self.nn_input_2.cuda(self.device_id)
+
+            D_upD_fb(self.optimizer, self.nn_input_1, self.nn_input_2, self.nn_network, self.batch_size, self.device_id)
 
 class queue_t:
     def __init__(self):
@@ -23,17 +65,4 @@ class queue_t:
     def dequeue(self):
         if len(self.impl) > 0:
             return self.impl.pop(0)
-        return None
-
-    def num_by_index(self, generator_index):
-        count = 0
-        for tsk in self.impl:
-            if tsk.generator_index == generator_index:
-                count += 1
-        return count
-
-    def dequeue_by_index(self, generator_index):
-        for i in range(len(self.impl)):
-            if self.impl[i].generator_index == generator_index:
-                return self.impl.pop(i)
         return None
