37f57dcded90b61f136f810c80f09231033a18ed
andyyang <r06922094@ntu.edu.tw>
2018-07-08 16:41:09 +0800

    example model: add parser and dataloader

--- example/model.py
+++ example/model.py
@@ -8,7 +8,42 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import argparse, os
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--batch_size', type=int, default=64, help='size of the batches')
+parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
+parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
+parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
+parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
+parser.add_argument('--channels', type=int, default=1, help='number of image channels')
+parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
+opt = parser.parse_args()
+
+
+def load_data():
+    os.makedirs('../data/mnist', exist_ok=True)
+    dataloader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data/mnist', train=True, download=True,
+                    transform=transforms.Compose([
+                        transforms.Resize(opt.img_size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                    ])),
+        batch_size=opt.batch_size, shuffle=True)
+
+    return dataloader
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
 
 class Generator(nn.Module):
     def __init__(self):
